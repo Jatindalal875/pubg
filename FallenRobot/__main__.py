6cad7fad4f4f628adf8ba8110be5ce0f87795338
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2023-02-24 17:24:15 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -218,7 +218,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAx0CaSNsewABE81XY_ik6Iy1CAZhlRmMBrqneAyZl3wAAvcHAAK7QshXG9qnHdH7MH4uBA"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
